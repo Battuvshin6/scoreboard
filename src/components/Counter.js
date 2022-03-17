8087a6626b639eda7894ce40c3283cebdebe4762
Battuvshin <mstarsacademy@MStarss-iMac.local>
2022-03-17 17:11:15 +0800

    update

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React from "react";
+import propTypes from "prop-types";
+const Counter = (props) => {
+  return (
+    <div className="counter">
+      <button
+        className="counter-action dicrement"
+        onClick={() => props.changeScore(-1, props.index)}
+      >
+        -
+      </button>
+      <span className="counter-score">{props.score}</span>
+      <button
+        className="counter-action increment"
+        onClick={() => props.changeScore(1, props.index)}
+      >
+        {" "}
+        +{" "}
+      </button>
+    </div>
+  );
+};
 
-const Counter = (props) =>{
-
-    return(
-        <div className='counter'>
-                <button className="counter-action dicrement" onClick={()=>props.changeScore(-1, props.index)}>-</button>
-                <span className='counter-score'>{ props.score }</span>
-                <button className="counter-action increment" onClick={()=>props.changeScore(1, props.index)}> + </button>
-            </div>
-    )
-}
-
-export default Counter
+Counter.protoTypes = {
+  index: propTypes.number,
+  score: propTypes.number,
+  changeScore: propTypes.func,
+};
+export default Counter;
+-
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,17 +1,26 @@
-import React from 'react'
-import Counter from './Counter'
+import React from "react";
+import Counter from "./Counter";
+import propTypes from "prop-types";
+import Crown from "./Crown";
+const Player = (props) => {
+  return (
+    <div className="player">
+      <div className="player-name">
+        <button
+          onClick={() => props.removePlayer(props.id)}
+          className="remove-player"
+        >
+          ✖
+        </button>
+        {props.name}
+      </div>
+      <Counter
+        index={props.index}
+        score={props.score}
+        changeScore={props.changeScore}
+      />
+    </div>
+  );
+};
 
-const Player = (props)=> {
-    return(
-        <div className="player">
-            
-            <div className="player-name">
-                <button onClick={() => props.removePlayer(props.id)} className="remove-player">✖</button>
-                {props.name}
-            </div>
-            <Counter index={props.index} score= {props.score} changeScore = {props.changeScore}/>
-        </div>
-    )
-  }
-
-export default Player
+export default Player;
